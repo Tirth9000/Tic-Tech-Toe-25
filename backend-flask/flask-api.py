bcf89361b6c97242d1a07ecc702923d67ebdcfe7
Tirth9000 <sharmatirth09012@gmail.com>
2025-04-12 22:58:53 +0530

    file modified

--- backend-flask/flask-api.py
+++ backend-flask/flask-api.py
@@ -1,8 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, session
 from http import HTTPStatus
 from flask_cors import CORS
 import pandas as pd, json
 import joblib, numpy as np
+from pymongo import MongoClient
+import pandasai as pai
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +18,16 @@ CORS(app)
 #     404: 'Not Found',
 #     500: 'Internal Server Error',
 # }
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client[""]
+collection = db[""]
+
+def execute_query(query=None, projection=None):
+    query = query or {}
+    result = list(collection.find(query, projection))
+    return result
+
 
 data = []
 @app.route('/api/data_trends', methods=["POST", "GET"])
@@ -42,6 +54,24 @@ def PredictFailure():
     return json.dumps({"probable": y_prob[0].tolist(), "predict": int(y_pred[0]), "status": HTTPStatus.OK})
 
 
+# @app.route('/upload-file', methods=['GET', 'POST'])
+# def FileLoader(): 
+#     data = request.json
+#     query_data = execute_query(query)
+#     session['file_address'] = query_data[0]["file"]
+#     return json.dumps({'message': "session stored successfully!", "status": 200})
+
+# @app.route('/chat-response', methods=['GET', 'POST'])
+# def ChatResponse():
+#     file_address = session.get('file_address')
+#     df = pai.read_csv()
+#     data = request.json
+#     if data:
+#         gen_response = df.chat(data+" And Provide the response as a single-line string statement instead of a structured format only.")
+#         response = json.dumps({"response": str(gen_response), "type": 'text'})
+#     else:
+#         response = json.dumps({"response": "Data not found!", "type": 'error', "status": False})
+#     return response
     
 
 if __name__ == '__main__':
