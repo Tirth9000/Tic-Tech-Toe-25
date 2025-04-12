d1f6a384c2000c2781e07490f64545f4a86911ce
RATHOD PRATIK VIJAYBHAI <22bt04120@gsfcuniversity.ac.in>
2025-04-12 19:15:44 +0530

    UI Update _ added charts

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, on, onMount } from "solid-js";
+import { createEffect, createSignal, on, onMount, Show } from "solid-js";
 import { Portal } from "solid-js/web";
 import { IconPlus } from "~/components/icons";
 import {
@@ -7,77 +7,115 @@ import {
 	CardDescription,
 	CardTitle,
 } from "~/components/ui/card";
+import { LineChart } from "~/components/ui/charts";
 import { addReadingsAsync } from "~/utils/dummyData";
 
 export const HomeComponent = () => {
+	const [temperatureData, setTemperatureData] = createSignal([24]);
+	const [vibrationData, setVibrationData] = createSignal([50]);
+	const [voltageData, setVoltageData] = createSignal([5.0]);
+	const [lastUpdate, setLastUpdate] = createSignal(
+		new Date().toLocaleTimeString(),
+	);
+	onMount(() => {
+		addReadingsAsync(
+			(val) =>
+				setTemperatureData((curr) =>{
+					if(curr.length<10){
+						curr.push(val)
+						return curr
+					}
+					const last9 = curr.slice(-9)
+					last9.push(val)
+					return last9
+				}),
+			42,
+			44
+		);
+		addReadingsAsync(
+			(val) =>
+				setVibrationData((curr) =>{	
+					if(curr.length<10){
+						curr.push(val)
+						return curr
+					}
+					const last9 = curr.slice(-9)
+					last9.push(val)
+					return last9
+				}),
+			42.1,
+			45.2
+		);
+		addReadingsAsync(
+			(val) =>
+				setVoltageData((curr) =>{
+					
+					if(curr.length<10){
+						curr.push(val)
+						return curr
+					}
+					const last9 = curr.slice(-9)
+					last9.push(val)
+					return last9
+				}),
+			4.98,
+			5.12
+		);
+	});
 
-	const [temperatureData, setTemperatureData] = createSignal([24])
-	const [vibrationData, setVibrationData] = createSignal([50])
-	const [voltageData, setVoltageData] = createSignal([5.0])
-	const [lastUpdate, setLastUpdate] = createSignal(new Date().toLocaleTimeString())
-	onMount(()=>{
-		addReadingsAsync((val)=>setTemperatureData(curr=>[...curr.slice(temperatureData().length-11), val]), 20, 40)
-		addReadingsAsync((val)=>setVibrationData(curr=>[...curr.slice(vibrationData().length-11), val]), 42, 55)
-		addReadingsAsync((val)=>setVoltageData(curr=>[...curr.slice(voltageData().length-11), val]), 4.48, 5.89)
-	})
-	
-	createEffect(()=>{
-		voltageData()
-		vibrationData()
-		temperatureData() 
-		setLastUpdate(new Date().toLocaleTimeString()) 
-	})
-	
+	createEffect(() => {
+		voltageData();
+		vibrationData();
+		temperatureData();
+		setLastUpdate(new Date().toLocaleTimeString());
+	});
+
 	return (
 		<div class="m-3">
 			<h2 class="text-lg">System Overview</h2>
-			<h4 class="text-gray-300 text-sm">Monitor your system health and statuses</h4>
+			<h4 class="text-gray-300 text-sm">
+				Monitor your system health and statuses
+			</h4>
 			<div class="flex gap-2 my-3 w-full">
-				<SystemStatsCard
-                    description="System Health"
-                    value='75 %'
-                />
-				<SystemStatsCard
-                    description="Active Sensors"
-                    value='4'
-                />
-				<SystemStatsCard
-                    description="Anomalies"
-                    value='1'
-                />
-				<SystemStatsCard
-                    description="Maintenance"
-                    value='0'
-                />
+				<SystemStatsCard description="System Health" value="75 %" />
+				<SystemStatsCard description="Active Sensors" value="4" />
+				<SystemStatsCard description="Anomalies" value="1" />
+				<SystemStatsCard description="Maintenance" value="0" />
 				<Card class="hover:shadow-current">
-					<CardDescription class="p-2 text-center">Add new node</CardDescription>
-					<CardContent class="place-items-center grid p-0"><IconPlus 
-						class='rounded-full size-6'
-					/></CardContent>
+					<CardDescription class="p-2 text-center">
+						Add new node
+					</CardDescription>
+					<CardContent class="place-items-center grid p-0">
+						<IconPlus class="rounded-full size-6" />
+					</CardContent>
 				</Card>
 			</div>
 			<div>Sensors</div>
 			<div class="gap-2 grid grid-cols-4">
 				<SensorCard
 					title="🌡️ Temperature"
-					value={temperatureData()[temperatureData().length-1]}
+					value={temperatureData()[temperatureData().length - 1]}
+					datasets={temperatureData()}
 					unit="°c"
 					lastUpdate={lastUpdate()}
 					status="NORMAL"
 				/>
 				<SensorCard
 					title="📳 Vibration"
-					value={vibrationData()[vibrationData().length-1]}
+					value={vibrationData()[vibrationData().length - 1]}
+					datasets={vibrationData()}
 					lastUpdate={lastUpdate()}
 					unit="Hz"
 					status="NORMAL"
 				/>
-				<SensorCard 
-				title="⚡ Voltage" 
-				value={voltageData()[voltageData().length-1]} 
-				unit="V" 	
-				lastUpdate={lastUpdate()}
-				status="NORMAL" />
+				<SensorCard
+					title="⚡ Voltage"
+					value={voltageData()[voltageData().length - 1]}
+					datasets={voltageData()}
+					unit="V"
+					lastUpdate={lastUpdate()}
+					status="NORMAL"
+				/>
 			</div>
 		</div>
 	);
@@ -86,27 +124,100 @@ export const HomeComponent = () => {
 const SystemStatsCard = (props) => {
 	return (
 		<Card class="p-2">
-			<CardDescription >{props.description}</CardDescription>
+			<CardDescription>{props.description}</CardDescription>
 			<CardContent class="p-2 text-center">{props.value}</CardContent>
 		</Card>
 	);
 };
 
 const SensorCard = (props) => {
+	const [show, setShow] = createSignal(false);
+	let modalRef;
+	let cardRef;
+
+	const popUpModal = () => {
+		setShow(true);
+		console.log("open");
+	};
+
+	onMount(() => {
+		document.addEventListener("click", (e) => {
+			console.log(cardRef?.contains(e.target) || e.target === cardRef);
+
+			if (cardRef?.contains(e.target) || e.target === cardRef)
+				return setShow(true);
+
+			return setShow(false);
+		});
+	});
+
 	return (
-		<Card class="p-2 hover:scale-105 transition-transform ease-in" style={{
-            "border-left": props.status==="NORMAL" ? "rgba(0,220,0, 0.5) 5px solid" :''
-        }}
+		<Card
+			ref={cardRef}
+			class="p-2 hover:scale-105 transition-transform ease-in"
+			style={{
+				"border-left":
+					props.status === "NORMAL" ? "rgba(0,220,0, 0.5) 5px solid" : "",
+			}}
+			onClick={() => popUpModal()}
 		>
-			<CardTitle class="">
-				{props.title}
-			</CardTitle>
+			<CardTitle class="">{props.title}</CardTitle>
 			<CardContent class="my-4 p-2 text-2xl">
 				{props.value} <span class="text-s">{props.unit}</span>
 			</CardContent>
 			<CardDescription class="text-xs">
 				Last updated at : {props.lastUpdate}{" "}
 			</CardDescription>
+			<Portal>
+				<Card
+					ref={modalRef}
+					class="absolute backdrop:bg-opacity-35"
+					style={{
+						opacity: show() ? "1" : "0",
+						"z-index": show() ? "1" : "-1",
+						"user-select": show() ? "inherit" : "true",
+						inset: "20vw",
+						top: show() ? "20px" : "0px",
+						transition: "all 500ms 1ms ease-in-out",
+					}}
+				>
+					<CardTitle class="p-2 uppercase"> {props.title}</CardTitle>
+					<CardContent>
+						<LineChart
+							data={{
+								labels: [
+									...Array.from({ length: 10 }, (v, i) =>
+										new Date(Date.now() - i * 1000).toLocaleTimeString(),
+									).reverse(),
+								],
+								datasets: [
+									{
+										label: props.unit,
+										data: props.datasets,
+									},
+								],
+							}}
+							options={{
+								animation: false, // 👈 this disables animations
+								responsive: true,
+								plugins: {
+									legend: {
+										display: false,
+									},
+								},
+								scales: {
+									x: {
+										display: true,
+									},
+									y: {
+										display: true,
+									},
+								},
+							}}
+						/>
+					</CardContent>
+				</Card>
+			</Portal>
 		</Card>
 	);
 };
--- front-end/src/utils/dummyData.js
+++ front-end/src/utils/dummyData.js
@@ -1,15 +1,22 @@
-const getDummyReadings = (min, max) =>{
+const getDummyReadings = (min, max, outlierProb = 0.01, outlierPeakDiff = max ) =>{
+    // console.log(outlierPeakDiff)
     return {
          sensor_id : 1,
-         values : Array.from({ length: 1000 }, () => (Math.random() * min + (max-min)).toFixed(2))
+         values : Array.from({ length: 100 }, () =>{ 
+            const random = Math.random()* (max-min) + min
+
+            const randomOutLierDelta = Math.random() >= 1-outlierProb ? Math.random()*max + outlierPeakDiff : 0 
+            return (random+randomOutLierDelta).toFixed(2) 
+        })
     }   
 }
-export const addReadingsAsync = async (callback, min, max, interval= 5000) =>{
-    const dummyReading = getDummyReadings(min, max)
-    console.log(dummyReading)
-    const id = setInterval(()=>{
+export const addReadingsAsync = async (callback, min, max, outlierProb = 0.01, outlierPeakDiff = max,  interval= 1000) =>{
+    const dummyReading = getDummyReadings(min, max,  outlierProb, outlierPeakDiff)
+    const interval_callback = ()=>{
         const entry = dummyReading.values.shift()
         if(!entry) clearInterval(id)
         callback(entry)
-    }, interval)
+    }
+    interval_callback()
+    const id = setInterval(interval_callback, interval)
 }
